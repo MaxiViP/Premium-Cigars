532d12eed49282469c3d109b48c83b67d6cf131b
MaximVPFIGO <polyakovmaksim058@gmail.com>
2025-11-26 11:26:33 +0300

    1

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,4 +2,4 @@ declare module '*.vue' {
   import { DefineComponent } from 'vue'
   const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>
   export default component
-}
+} //ssdd
